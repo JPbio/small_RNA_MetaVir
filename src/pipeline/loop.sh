3c72b2df6715e0d4a37b7238675b61425d938d13
hjcostabr76 <hjcosta.dev@gmail.com>
2023-08-16 08:24:54 -0300

    feat: pipeline loop
    adding sh file to loop over files running the pipeline on each one

--- src/pipeline/loop.sh
+++ src/pipeline/loop.sh
@@ -1,11 +1,9 @@
-# ls /small-rna-metavir/asset/libs/SRA_smallRNA/batch-01/*.fasta
-
 skip_libs=("")
 dir_root_libs=""
 dir_root_runs=""
 
-for file in `ls ${dir_root_libs}/*.fasta`
 # for file in `ls ${dir_root_libs}/`
+for file in `ls ${dir_root_libs}/*.fasta`
 do
 
 lib_id=`echo $file | cut -f 7 -d "/" | cut -f 1 -d "_"`
@@ -28,15 +26,16 @@ fi
 
 # Create results directory
 exec_id=val-${lib_id}
-dir_run=${dir_root_runs}/${exec_id}/
+dir_run=${dir_root_runs}/${exec_id}
+# echo "lib_id: '${lib_id}' | exec_id: '${exec_id}' | dir_run: '${dir_run}'"
 
 if [ ! -d "$dir_run" ]; then
+    # echo mkdir $dir_run
     mkdir $dir_run
 fi
 
 # Run pipeline for the current lib
-# echo "perl main.pl -fasta ${file} -hostgenome /small-rna-metavir/asset/refs/ref_hosts/refs_Aae_Aalb_Agam_culex.fasta -process 40 -si 15 -se 35 -size 20000 -exec-id val-${lib_id} -prefix ${lib_id} 2> ./runs/val-${lib_id}/val-${lib_id}.err.log"
 # echo "perl main.pl -fasta ${file} -hostgenome /small-rna-metavir/asset/refs/ref_hosts/refs_Aae_Aalb_Agam_culex.fasta -process 40 -si 15 -se 35 -size 20000 -exec-id ${exec_id} -prefix ${lib_id} 2> ${dir_run}/${exec_id}.err.log"
-
 perl main.pl -fasta ${file} -hostgenome /small-rna-metavir/asset/refs/ref_hosts/refs_Aae_Aalb_Agam_culex.fasta -process 40 -si 15 -se 35 -size 20000 -exec-id ${exec_id} -prefix ${lib_id} 2> ${dir_run}/${exec_id}.err.log
+
 done
